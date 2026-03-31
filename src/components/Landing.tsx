b70dbb60619bca1c8f7624748e1ff739421f87f9
prashant9430 <prashantkumarpandey9430@gmail.com>
2026-03-31 11:26:19 +0530

    Update name and title in Landing component

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -9,19 +9,19 @@ const Landing = ({ children }: PropsWithChildren) => {
           <div className="landing-intro">
             <h2>Hello! I'm</h2>
             <h1>
-              RAJESH
+              PRASHANT
               <br />
-              <span>CHITYAL</span>
+              <span>PANDEY</span>
             </h1>
           </div>
           <div className="landing-info">
             <h3>A Full Stack</h3>
             <h2 className="landing-info-h2">
               <div className="landing-h2-1">Developer</div>
-              <div className="landing-h2-2">Engineer</div>
+              <div className="landing-h2-2">Jyotish Pandit</div>
             </h2>
             <h2>
-              <div className="landing-h2-info">Engineer</div>
+              <div className="landing-h2-info">Jyotish Pandit</div>
               <div className="landing-h2-info-1">Developer</div>
             </h2>
           </div>
